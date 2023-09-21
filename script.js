be50dfc672633847c0bc6c3e5fc2f06f45ceeae4
Kristian Grøsvik <kristian.grosvik04@gmail.com>
2023-09-21 13:28:42 +0200

    21-09-23

--- script.js
+++ script.js
@@ -1,39 +1,106 @@
-let question = [
-    "Høy skatt er bra for samfunnet",
-    "Det er viktig å bevare naturen",
-    "Det er viktig å bevare kulturen",
+// let question = [
+//     "Høy skatt er bra for samfunnet",
+//     "Det er viktig å bevare naturen",
+//     "Det er viktig å bevare kulturen",
+// ]
+
+
+const test = [
+    {
+        question: "Høy skatt er bra for samfunnet",
+        heltEnig:{
+            Høyre: 0,
+            Rødt: 2,
+            Frp : 0,
+        },
+        Littenig: {
+            Høyre: 0,
+            Rødt: 1,
+            Frp: 0,
+        },
+        Littuenig: {
+            Høyre: 2,
+            Rødt: 0,
+            Frp: 1,
+        },
+        heltUenig: {
+            Høyre: 1,
+            Rødt: 0,
+            Frp: 2,
+        }
+
+    }
 ]
 
+// Definerer i som 0 og skriver ut første spørsmål
+let i = 0;
+document.getElementById("SpørsNum").innerText = "Spørsmål " + (i + 1) + " av " + test.length + ":";
+document.getElementById("question").innerText = test[i].question;
 
-i = 0;
+// Navigering fram mellom spørsmål
+document.getElementById("btnNext").addEventListener("click", function (){
+        console.log(i);
+        console.log("test length " + test.length);
+        //Sjekker om det er siste spørsmål
+        if(i == test.length) {
+            return;
+        }
+        else if(i == test.length -1) {
+            // Hvis det er siste spørsmål, gå til resultat siden
+            
+        }       
+        // plusser i og skriver ut neste spørsmål
+        i++;
+        document.getElementById("question").innerText = test[i].question;
+        document.getElementById("SpørsNum").innerText = "Spørsmål " + (i + 1) + " av " + test.length + ":";
+        if(localStorage.getItem(test[i].question)){
+            document.getElementById(localStorage.getItem(test[i].question)).checked = true;
+        }
 
-document.getElementById("question").innerText = question[i];
+});
+// Fjerner dette fordi det lager for mye problemer
+// // Navigering bakover mellom spørsmål
+// document.getElementById("btnBack").addEventListener('click', function () {
+//     if(i <= 0) {
+//         console.log("Du er på første spørsmål");
+//        return;
+//     }
+//     i--;
+//     document.getElementById("question").innerText = test[i].question;
+//     document.getElementById("SpørsNum").innerText = "Spørsmål " + (i + 1) + " av " + test.length + ":";
 
 
+//     if(localStorage.getItem(test[i].question)){
+//         document.getElementById(localStorage.getItem(test[i].question)).checked = true;
+//     }
+// });
 
-document.getElementById("btnNext").onclick = function () {
-    if(i == question.length) {
-        return;
-    }
-    else if(i == question.length -1) {
-        return;
-    }
-    console.log(i);
-    i++;
-    document.getElementById("question").innerText = question[i];
+// Sjekker om du har svart på spørsmålet
+document.getElementById("HeltUenig").addEventListener("click", function () {
+    let svar = "HeltUenig";
+    console.log("HeltUenig");
+    checkChoice(svar)
+});
+document.getElementById("Uenig").addEventListener("click", function () {
+    let svar = "Uenig";
+    console.log("Uenig");
+    checkChoice(svar)
 
-    svar = document.querySelector("input:checked").value
-    console.log(svar);
+});
+document.getElementById("LittEnig").addEventListener("click", function () {
+    let svar = "LittEnig";
+    console.log("Enig");
+    checkChoice(svar)
+});
+document.getElementById("HeltEnig").addEventListener("click", function () {
+    let svar = "HeltEnig";
+    console.log("HeltEnig");
+    checkChoice(svar)
+});
+
+function checkChoice(svar) {
+    localStorage.setItem(test[i].question, svar);
+
 }
 
-document.getElementById("btnBack").onclick = function () {
-    if(i <= 0) {
-        console.log("kys");
-       return;
-    }
-    i--;
-    document.getElementById("question").innerText = question[i];
-    svar = document.querySelector("input:checked").value;
-    console.log(i);
-    console.log(svar);
-}
+
